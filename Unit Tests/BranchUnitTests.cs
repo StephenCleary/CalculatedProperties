df550ae3c6352ba7d44a434481acd6b9a11ad99e
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-04-24 21:43:11 -0400

    More unit tests.

---
+++ Unit Tests/BranchUnitTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class BranchUnitTests
+    {
+        public sealed class BranchViewModel : ViewModelBase
+        {
+            public bool UseB
+            {
+                get { return Properties.Get(false); }
+                set { Properties.Set(value); }
+            }
+
+            public int A
+            {
+                get { return Properties.Get(7); }
+                set { Properties.Set(value); }
+            }
+
+            public int B
+            {
+                get { return Properties.Get(11); }
+                set { Properties.Set(value); }
+            }
+
+            public int CalculatedValue
+            {
+                get
+                {
+                    return Properties.Calculated(() =>
+                    {
+                        ++CalculatedValueExecutionCount;
+                        return UseB ? B : A;
+                    });
+                }
+            }
+
+            public int CalculatedValueExecutionCount;
+        }
+        
+        [TestMethod]
+        public void Calculated_InitialValueIsCalculated()
+        {
+            var vm = new BranchViewModel();
+            Assert.AreEqual(7, vm.CalculatedValue);
+            Assert.AreEqual(1, vm.CalculatedValueExecutionCount);
+        }
+
+        [TestMethod]
+        public void IndependentPropertyChanges_DoesNotRaisePropertyChangedForCalculated()
+        {
+            var changes = new List<string>();
+            var vm = new BranchViewModel();
+            vm.PropertyChanged += (_, args) => changes.Add(args.PropertyName);
+            var value = vm.B;
+            value = vm.CalculatedValue;
+            vm.B = 13;
+            CollectionAssert.AreEquivalent(new[] { "B" }, changes);
+        }
+
+        [TestMethod]
+        public void DependentPropertyChanges_RaisesPropertyChangedForCalculated()
+        {
+            var changes = new List<string>();
+            var vm = new BranchViewModel();
+            vm.PropertyChanged += (_, args) => changes.Add(args.PropertyName);
+            var value = vm.A;
+            value = vm.CalculatedValue;
+            vm.A = 13;
+            CollectionAssert.AreEquivalent(new[] { "A", "CalculatedValue" }, changes);
+        }
+
+        [TestMethod]
+        public void Calculated_AfterBranchSwitch_IsRecalculated()
+        {
+            var vm = new BranchViewModel();
+            var value = vm.CalculatedValue;
+            vm.UseB = true;
+            Assert.AreEqual(11, vm.CalculatedValue);
+            Assert.AreEqual(2, vm.CalculatedValueExecutionCount);
+        }
+
+        [TestMethod]
+        public void IndependentPropertyChanges_AfterBranchSwitch_DoesNotRaisePropertyChangedForCalculated()
+        {
+            var changes = new List<string>();
+            var vm = new BranchViewModel();
+            vm.PropertyChanged += (_, args) => changes.Add(args.PropertyName);
+            var value = vm.CalculatedValue;
+            vm.UseB = true;
+            value = vm.CalculatedValue;
+
+            vm.A = 13;
+
+            CollectionAssert.AreEquivalent(new[] { "UseB", "CalculatedValue", "A" }, changes);
+        }
+
+        [TestMethod]
+        public void DependentPropertyChanges_AfterBranchSwitch_RaisesPropertyChangedForCalculated()
+        {
+            var changes = new List<string>();
+            var vm = new BranchViewModel();
+            vm.PropertyChanged += (_, args) => changes.Add(args.PropertyName);
+            var value = vm.CalculatedValue;
+            vm.UseB = true;
+            value = vm.CalculatedValue;
+
+            vm.B = 13;
+
+            CollectionAssert.AreEquivalent(new[] { "UseB", "CalculatedValue", "B", "CalculatedValue" }, changes);
+        }
+    }
+}
---
+++ Unit Tests/ChainUnitTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class ChainUnitTests
+    {
+        public sealed class ChainViewModel : ViewModelBase
+        {
+            public int Leaf
+            {
+                get { return Properties.Get(7); }
+                set { Properties.Set(value); }
+            }
+
+            public int Branch
+            {
+                get { return Properties.Get(11); }
+                set { Properties.Set(value); }
+            }
+
+            public int Intermediate
+            {
+                get
+                {
+                    return Properties.Calculated(() =>
+                    {
+                        ++IntermediateExecutionCount;
+                        return Leaf * 2;
+                    });
+                }
+            }
+
+            public int Root
+            {
+                get
+                {
+                    return Properties.Calculated(() =>
+                    {
+                        ++RootExecutionCount;
+                        return Intermediate + Branch;
+                    });
+                }
+            }
+
+            public int RootExecutionCount;
+            public int IntermediateExecutionCount;
+        }
+        
+        [TestMethod]
+        public void Root_InitialValueIsCalculated()
+        {
+            var vm = new ChainViewModel();
+            Assert.AreEqual(25, vm.Root);
+            Assert.AreEqual(1, vm.IntermediateExecutionCount);
+            Assert.AreEqual(1, vm.RootExecutionCount);
+        }
+
+        [TestMethod]
+        public void LeafChanges_RaisesPropertyChangedForAllAffectedProperties()
+        {
+            var changes = new List<string>();
+            var vm = new ChainViewModel();
+            vm.PropertyChanged += (_, args) => changes.Add(args.PropertyName);
+            var value = vm.Root;
+            vm.Leaf = 13;
+            CollectionAssert.AreEquivalent(new[] { "Leaf", "Intermediate", "Root" }, changes);
+        }
+    }
+}
