b84e05248d227c3c551b3d0fb58bb8b6b20f789c
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2015-04-02 19:13:07 -0400

    Moved .NET Core dev to its own repo.

--- NETCore/Coverage.ps1
+++
@@ -1,2 +0,0 @@
-$artifactLocation = 'artifacts\bin\Nito.CalculatedProperties\Debug\net45'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/master/Coverage.ps1'))
