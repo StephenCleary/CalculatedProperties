93ad894c9b5bb9befb7784ee7e52fab52eb33b39
Nick Dunets <nick.dunets@vulcansteel.co.nz>
2017-06-29 01:40:32 +1200

    1.0.1 increase version

--- CalculatedProperties/Properties/AssemblyInfo.cs
+++ CalculatedProperties/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCompany("Stephen Cleary")]
 [assembly: AssemblyDescription("Easy-to-use calculated properties for MVVM apps (.NET 4, MonoTouch, MonoDroid, Windows 8, Windows Phone 8.1, Windows Phone Silverlight 8.0, and Silverlight 5).")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
